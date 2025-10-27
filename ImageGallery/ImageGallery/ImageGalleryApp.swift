90caae658fc1d28b36c24348b24288b815dd215b
Kartikay Rane <kartikayranework@gmail.com>
2025-10-27 22:56:35 +0530

    Initialize ImageGallery SwiftUI project
    Set up a new Xcode project for ImageGallery with basic SwiftUI structure, including ContentView and ImageGalleryApp entry point. Added asset catalogs and initial project configuration files.

---
+++ ImageGallery/ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Kartikay Rane on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
